8c5ef506c828b26971d711241418cebf29925c17
Ilya <ilyaniafiodau@Ilyas-MacBook-Pro.local>
2025-01-08 23:03:03 +0300

    homework 2 complete

--- app/views.py
+++ app/views.py
@@ -1,69 +1,182 @@
 import copy
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.core.paginator import Paginator
-
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.template import loader
 
 # Create your views here.
 
+pop_tags = ["Why", "How", "sigh", "duh", "WTF", "OK", "idk"]
+question_tags = ["WTF", "idk"]
+
+top_members = ["Mr Why", "Mr Who", "Mr When", "Dr Know-It-All", "Armchair Sportsman"]
 
 questions = []
 for i in range(0,30):
   questions.append({
-    'title': 'Question#'  + str(i),
+    'title': 'Question #'  + str(i),
     'id': i,
-    'text': 'Some lorem ipsum kinda text to provide content for question' + str(i)
+    'text': 'Some lorem ipsum kinda text to provide content for what-is-supposed-to-be a complicated question ' + str(i)
+  })
+
+  hot_questions = copy.deepcopy(questions)
+  hot_questions.reverse()
+
+answers = []
+for i in range(0,2):
+  answers.append({
+    'title': 'Answer #'  + str(i),
+    'id': i,
+    'text': 'Some lorem ipsum kinda text to provide content for answer ' + str(i)
   })
 
 
+  from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+def paginate(objects_list, request, per_page = 4):
+    page_number = request.GET.get('page', 1)  
+
+    paginator = Paginator(objects_list, per_page)  
+    
+    try:
+        page = paginator.get_page(page_number) 
+    except PageNotAnInteger:
+        page = paginator.get_page(1)
+    except EmptyPage:
+        page = paginator.get_page(paginator.num_pages)
+
+    return page, page.number
+    
+
 def index(request):
-    page_num = int(request.GET.get('page', 1))
-    paginator = Paginator(questions, 5)
-    page = paginator.page(page_num)
+    page, page_number = paginate(questions, request, per_page = 4)
+    if (page is None):
+        return redirect(f"/?page={page_number}")
+
     return render(
-       request, 'index.html', context = {'questions': page.object_list, 'page_obj': page }
-       )
+        request,
+        template_name="index.html",
+        context={
+            'page_obj': page,
+            'questions': page.object_list,
+            'question_tags': question_tags,
+            'pop_tags': pop_tags,
+            'top_users': top_members
+        }
+    )
 
 def hot(request):
-   hot_questions = copy.deepcopy(questions)
-   hot_questions.reverse()
-   return render(
-      request, 'hot.html', context = {'questions': hot_questions}
-      )
+    page, page_number = paginate(hot_questions, request, per_page = 4)
+    if (page is None):
+        return redirect(f"/?page={page_number}")
 
-def tag(request, question_tag):
-   tagged_questions = copy.deepcopy(questions)
-   tagged_questions.remove(question[0])
-   return render(
-      request, 'tag.html', context = {'questions': tagged_questions}
-      )
+    return render(
+        request,
+        template_name="hot.html",
+        context={
+            'page_obj': page,
+            'questions': page.object_list,
+            'question_tags': question_tags,
+            'pop_tags': pop_tags,
+            'top_users': top_members
+        }
+    )
 
 def question(request, question_id):
-   single_question = questions[question_id]
-   return render(
-      request, 'single_question.html', context = {'question': single_question}
-      )
+    if (question_id < 0) or (question_id >= len(questions)):
+        return render(
+                        request, 
+                        'question_not_found.html',
+                        status=404,
+                        context={
+                            'pop_tags': pop_tags,
+                            'top_users': top_members
+                        }
+                    )
+    
+    return render(
+        request,
+        template_name="question.html",
+        context={
+            'question': questions[question_id],
+            'pop_tags':pop_tags,
+            'question_tags': question_tags,
+            'top_users': top_members,
+            'answers': answers
+        }
+    )
 
-def login(request):
-   login_error = {
-        'error_message': None, 
-    }
-   return render(
-      request, 'login.html', context = login_error
-      )
+def tag(request, tag_name):
+    page, page_number = paginate(questions, request, per_page=4)
+    if (page is None):
+        return redirect(f"/?page={page_number}")
+
+    return render(
+        request,
+        template_name="tag.html",
+        context={
+            'page_obj': page,
+            'questions': page.object_list,
+            'question_tags': question_tags,
+            'tag_name': tag_name,
+            'pop_tags': pop_tags,
+            'top_users': top_members
+        }
+    )
 
 def signup(request):
-   signup_error = {
-        'error_message': None, 
-    }
    return render(
-      request, 'login.html', context = signup_error
+      request, 'signup.html',
+      context={
+            'pop_tags': pop_tags,
+            'top_users': top_members,
+            'question_tags': question_tags,
+        }
       )
 
 def ask(request):
-   ask_error = {
-        'error_message': None, 
-    }
    return render(
-      request, 'login.html', context = ask_error
+      request, 'ask.html',
+      context={
+            'pop_tags': pop_tags,
+            'top_users': top_members,
+            'question_tags': question_tags,
+        }
       )
+
+def settings(request):
+   return render(
+      request, 'settings.html',
+      context={
+            'pop_tags': pop_tags,
+            'top_users': top_members,
+            'question_tags': question_tags,
+        }
+      )
+
+def login(request):
+  #if request.method == 'POST':
+      #username = request.POST.get('username')
+     # password = request.POST.get('password')
+      #confirm = request.POST.get('confirm')
+      return render(
+          request, 'login.html',
+          context={
+            'pop_tags': pop_tags,
+            'top_users': top_members,
+            'question_tags': question_tags,
+            }
+          )
+
+#not yet implemented functions
+def logout(request):
+   return redirect(reverse('index') + "?after=logout")
+
+def users(request):
+   return render(request, 'users.html')
+
+def profile_edit(request):
+   return render(request, 'profile/edit.html')
+
+
+
--- askme_nefyodov/urls.py
+++ askme_nefyodov/urls.py
@@ -19,12 +19,13 @@ from django.urls import path
 from django.contrib import admin
 
 urlpatterns = [
-    path('', views.index, name = "index"),
-    path('hot/', views.hot, name = "hot"),
-    path('/tag/blablabla/', views.tag, name = "tag"),
-    path('question/<int:question_id>', views.question, name = "single_question"),
-    path('login/', views.login, name = "login"),
-    path('signup/', views.signup, name = "signup"),
-    path('ask/', views.ask, name = "ask"),
+    path('', views.index, name="index"),
+    path('hot/', views.hot, name="hot"),
+    path('tag/<tag_name>', views.tag, name="tag"),
+    path('question/<int:question_id>', views.question, name="question"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('ask/', views.ask, name="ask"),
     path('admin/', admin.site.urls),
+    path('settings/', views.settings, name="settings"),
 ]
